f2961a9fafb3c30a9e9a73c144d564a663e0bb3c
Paul_Fro <paul.frommelt@stud.tu-darmstadt.de>
2023-05-04 21:54:17 +0200

    new test

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ import unittest
 
 import config_helper
 import excel_helper
+from GUI_config_categories import *
 
 class ExampleTest(unittest.TestCase):
 
@@ -17,6 +18,13 @@ class ExcelTest(unittest.TestCase):
         
         excel_helper.export_new_categories(categories_new, path_excel)
         
+class ConfigGUITest(unittest.TestCase):
+    
+    def test_startup(self):
+        path_excel = config_helper.load_config()["default_path"]
+        found_categories = {"Test": ["Keyword_1", "Test2"], "Lebensmittel": ["Test"]}
+        
+        GUI_config_categories(found_categories, path_excel)
         
 
 
