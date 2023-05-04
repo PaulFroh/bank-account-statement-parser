a0cbcb99c56f7364b156fb384ad060b991cee903
Titanic09 <105213448+Titanic09@users.noreply.github.com>
2023-05-05 00:43:51 +0200

    Added the found_categories to the gui

--- GUI_config_categories.py
+++ GUI_config_categories.py
@@ -5,35 +5,43 @@ class GUI_config_categories:   # definition of the class
         def __init__(self, found_categories, path_excel):
                 self.found_categories = found_categories
                 self.root = tk.Tk()
-                self.root.resizable(0,0)    # Window is not ajustabl
-                self.root.geometry("350x350")
+                #self.root.resizable(0,0)    # Window is not ajustabl
+                self.root.geometry("1000x800")
                 self.root.title("Categories-Configurator")
-                
+                # Create a Grid-Layout
+                for i in range(10):
+                        for j in range(10):
+                                tk.Label(self.root).grid(row=i, column=j)
                 # Headline
                 self.label_headline = tk.Label(self.root, text="Categories Custom-Settings", font=('Arial', 18))   # adds a Label above the entry to let people know what the programm expects
-                self.label_headline.grid(column=1, row=1, padx="20", pady="50")
+                self.label_headline.grid(column=5, row=0, padx="20", pady="50")
 
                 # Lable which tells the user that new categories were found 
                 self.label_new = tk.Label(self.root, text="We found following new categories:", font=('Arial',11))   # adds a Label so people know what the programm expects
-                self.label_new.grid(column=1, row=2, padx="10", pady="10")
+                self.label_new.grid(column=5, row=1, padx="10", pady="10")
 
                 # FOR-LOOP --> Label area where the new categories are shown with checkboxes
                 index_category = 0
                 for category in found_categories:
                         
-                        self.label = tk.Label(self.root, text=category)                     # creates a label with the first key of the "i-th sublist"
-                        self.label.grid(column=1, row=[2+index_category], padx="5", pady="1", sticky=tk.W)
+                        self.label = tk.Label(self.root, text=category, font=('Arial',10, 'bold'))                     # creates a label with the first key of the "i-th sublist"
+                        self.label.grid(column=[index_category], row=[2], padx="10", pady="1", sticky=tk.W)
                         
                         index_keyword = 0
                         for keyword in found_categories[category]:                                      # creates a label with all elements of the specific sublist
-                                self.label = tk.Label(self.root, text=keyword)
-                                self.label.grid(column=1, row=[index_category+index_keyword+2], padx="5", pady="1", sticky=tk.W)
+                                self.label = tk.Label(self.root, text=keyword, font=('Arial',10))
+                                self.label.grid(column=[index_category], row=[index_keyword+3], padx="10", pady="1", sticky=tk.W)
                                 index_keyword += 1
-                        index_category += 1   
+                        index_category += 1
+
+                # ADD CHECKBOXES TO THE CATEGORIES AND KEYWORDS
+                # INDENT THE SUB CATEGORIES
+                # CREATE FRAMES FOR DIFFERENT PARTS: ONE FOR THE HEADLINE THE STATEMENT THE QUESTION AND THE BUTTONS AND ANOTHER ONE FOR THE CHECKBOXES AND THE CATEGORIES
+                # ADD A CHECKBOX "CHOOSE ALL SUGGESTIONS"
 
                 # Lable which asks you if you want to add the categories and keywords to the excel file
                 self.label_transfer = tk.Label(self.root, text="Do you want to add the new categories?", font=('Arial',11))   # adds a Label so people know what the programm expects
-                self.label_transfer.grid(column=1, row=4, padx="10", pady="10")
+                self.label_transfer.grid(column=4, row=[80], padx="10", pady="10")
 
 
 
@@ -42,11 +50,11 @@ class GUI_config_categories:   # definition of the class
 
                 # button for NOT adding the new categories to the excel /transfer the data
                 self.button_dont = tk.Button(self.root, text="No", relief="groove", font=('Arial',11), bg="lightgrey", command=self.quit)  # if you klick on the button the funktion transfer data will be passed and called whenever the button is klicked
-                self.button_dont.grid(column=1, row=5, padx="10",sticky=tk.W, pady="50")
+                self.button_dont.grid(column=4, row=[81], padx="10",sticky=tk.W, pady="50")
 
                 # button for adding the new categories to the excel /transfer the data
                 self.button_do = tk.Button(self.root, text="Yes", relief="groove", font=('Arial',11), bg="lightgrey", command=self.transfer_categories)  # if you klick on the button the funktion transfer data will be passed and called whenever the button is klicked
-                self.button_do.grid(column=1, row=5, padx="10",sticky=tk.E, pady="50")
+                self.button_do.grid(column=4, row=[81], padx="10",sticky=tk.E, pady="50")
 
                 
                 self.root.mainloop()
--- tests/test.py
+++ tests/test.py
@@ -22,7 +22,7 @@ class ConfigGUITest(unittest.TestCase):
     
     def test_startup(self):
         path_excel = config_helper.load_config()["default_path"]
-        found_categories = {"Test": ["Keyword_1", "Test2"], "Lebensmittel": ["Test"]}
+        found_categories = {"Test": ["Keyword_1", "Test2"], "Lebensmittel": ["Test"], "Discounter": ["Netto", "Aldi", "Penny"]}
         
         GUI_config_categories(found_categories, path_excel)
         
