20b9bfdf416ffd19068f2118346992ef6eb2a0b6
Paul_Fro <paul.frommelt@stud.tu-darmstadt.de>
2023-05-06 11:12:33 +0200

    Test Frame to check if everything works

--- GUI_config_categories.py
+++ GUI_config_categories.py
@@ -1,131 +1,112 @@
 import tkinter as tk
-import excel_helper
+from tkinter import ttk
 
-class GUI_config_categories:   # definition of the class
-        def __init__(self, found_categories, path_excel):
-                self.found_categories = found_categories
-                self.path_excel = path_excel
+class CategorieGUI():
+    
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.geometry("800x550")
+        self.root.title("Categories-Configurator")
+        self.found_categories = {"Test": ["Keyword_1", "Test2"], "Lebensmittel": ["Test"], "Discounter": ["Netto", "Aldi", "Penny"], "Pornohefte": ["Playboy"], "Dümmste Mitbewohner": ["Jean", "Maria", "Aldi","Lange","Liste","Hallo"], "Test5": ["Keyword_4", "Test3"], "Lebensmittellager": ["Testen"], "Lebensmitteltest": ["Testo"], "Discounterpreise": ["Netto Markendiscount", "Aldipuh", "PennyDenny"], "Pornoheftelecker": ["Playboyman"], "Dümmste Eis": ["Joa", "Mario", "Alda"], "Test8": ["Keyword_10", "Test100"]}
+        # Outer frames
+        frame1 = tk.Frame(self.root, height=75, width=800, bd=2, relief="groove")
+        frame1.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-                self.root = tk.Tk()
-                #self.root.resizable(0,0)    # Window is not ajustabl
-                self.root.geometry("800x550")
-                self.root.resizable(width=0, height=0)
-                self.root.title("Categories-Configurator")
+        frame3 = tk.Frame(self.root, height=75, width=800)
+        frame3.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
-                # Create a Grid-Layout for the window
-                for i in range(3):
-                        for j in range(1):
-                                tk.Label(self.root).grid(row=i, column=j)
+        # Outer Content
+        # Headline
+        label_headline = tk.Label(frame1, text="Categories Custom-Settings", font=("Arial", 23, 'bold'),  fg="black")
+        label_headline.pack(side=tk.TOP, pady=30)
 
-                # Create a frame for the top of the window
-                self.frame_headline = tk.Frame(self.root, width = 800, height = 200, background = "#e68d00")
-                # Create a Grid-Layout for this specific frame
-                for i in range(3):
-                        for j in range(3):
-                                tk.Label(self.frame_headline).grid(row=i, column=j)
-                self.frame_headline.grid(row = 0, column = 1)
+        # Checkbox für alle Kategorien
+        select_all_var = tk.IntVar()
+        select_all_checkbox = tk.Checkbutton(frame3, text="Select All", font=("Arial", 10, 'bold'), variable=select_all_var, command=lambda: select_all(select_all_var.get()))
+        select_all_checkbox.pack(side=tk.LEFT, padx=40)
+
+        # Middle frame with scrollbar
+        frame2 = tk.Frame(self.root, bd=2, relief="groove")
+        frame2.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        self.canvas = tk.Canvas(frame2, bg="white")
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        scrollbar = ttk.Scrollbar(frame2, orient=tk.VERTICAL, command=self.canvas.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+
+        # Inner content
+        self.inner_frame = tk.Frame(self.canvas, bg="white")
+
+        # Funktion zum Auswählen/Abwählen aller Checkboxen in frame2
+        def select_all(value):
+            for widget in self.inner_frame.winfo_children():
+                if isinstance(widget, tk.Checkbutton):
+                    widget.deselect()
+                    if value == 1:
+                        widget.select()
+
+        # Lable which asks you if you want to add the categories and keywords to the excel file
+        label_transfer = tk.Label(frame3, text="Add the new categories?", font=('Arial',11, 'bold'))
+        label_transfer.pack(side=tk.TOP, padx="80", pady="10", anchor='ne')
+
+        # button for NOT adding the new categories to the excel /transfer the data
+        button_dont = tk.Button(frame3, text="No", relief="groove", font=('Arial',11), bg="lightgrey", width=10, command=self.quit)
+        button_dont.pack(side=tk.RIGHT, padx="50", pady="10")
 
 
+        # button for adding the new categories to the excel /transfer the data
+        button_do = tk.Button(frame3, text="Yes", relief="groove", font=('Arial',11), width=10, bg="lightgrey", command=self.transfer_categories)
+        button_do.pack(side=tk.RIGHT, padx="10", pady="10")
+        #button_do.config(command=transfer_categories)
 
 
-                # Create a frame for the middle of the window
-                self.frame_checkboxes = tk.Frame(self.root, width = 800, height = 100, background = "#ffc600")
-                # Create a Grid-Layout for this specific frame
-                for i in range(10):
-                        for j in range(10):
-                                tk.Label(self.root).grid(row=i, column=j)
-                self.frame_checkboxes.grid(row = 1, column = 1)
+        # LOOPS which create as many checkbuttons as categories and sub categories 
+        index_category = 0
+        row_index = 0  # Zeilenindex für die Anordnung der Checkboxen
+        for category in self.found_categories:
+            check_state = tk.IntVar()
+            check = tk.Checkbutton(self.inner_frame, text=category, font=('Arial',10, 'bold'), variable=check_state)
+            check.grid(row=row_index, column=(index_category%4)*2, padx="5", pady="5", sticky=tk.W)
 
-
-
-
-                # Create a frame for the bottom of the window
-                self.frame_buttons = tk.Frame(self.root, width = 800, height = 100, background = "#e68d00")
-                # Create a Grid-Layout for this specific frame
-                for i in range(10):
-                        for j in range(10):
-                                tk.Label(self.root).grid(row=i, column=j)
-                self.frame_buttons.grid(row = 2, column = 1)
-
-                # Headline
-                self.label_headline = tk.Label(self.frame_headline, text="Categories Custom-Settings", justify='center', font=('Arial', 18))   # adds a Label above the entry to let people know what the programm expects
-                self.label_headline.grid(column=5, row=0, padx="20", pady="50")
-
-                # Lable which tells the user that new categories were found 
-                self.label_new = tk.Label( self.frame_headline, text="We found following new categories:", font=('Arial',11))   # adds a Label so people know what the programm expects
-                self.label_new.grid(column=5, row=1, padx="10", pady="10")
-
-                # FOR-LOOP --> Label area where the new categories are shown with checkboxes
-                
-                self.keyword_checkboxes = {}
-                index_category = 0
-                for category in found_categories:
-                        
-                        check_state = tk.IntVar()
-                        self.check = tk.Checkbutton(self.frame_checkboxes, text=category, font=('Arial',10, 'bold'), variable=check_state)
-                        self.check.grid(column=[index_category], row=[2], padx="5", pady="5", sticky=tk.W)
-                        
-                        self.keyword_checkboxes[category] = []
-                        
-                
-                        
-                        index_keyword = 0
-                        for keyword in found_categories[category]:                                      # creates a label with all elements of the specific sublist
-                                
-                                check_state = tk.IntVar()
-                                self.check = tk.Checkbutton(self.frame_checkboxes, text=keyword, font=('Arial',10), variable=check_state)
-                                self.check.grid(column=[index_category], row=[index_keyword+3], padx="5", pady="5", sticky=tk.W)
-                                
-                                self.keyword_checkboxes[category].append((keyword, check_state))
-
-                                index_keyword += 1
-                        index_category += 1
-
-                # ADD CHECKBOXES TO THE CATEGORIES AND KEYWORDS
-                # INDENT THE SUB CATEGORIES
-                # CREATE FRAMES FOR DIFFERENT PARTS: ONE FOR THE HEADLINE THE STATEMENT THE QUESTION AND THE BUTTONS AND ANOTHER ONE FOR THE CHECKBOXES AND THE CATEGORIES
-                # ADD A CHECKBOX "CHOOSE ALL SUGGESTIONS"
-
-                # Lable which asks you if you want to add the categories and keywords to the excel file
-                self.label_transfer = tk.Label(self.frame_buttons, text="Do you want to add the new categories?", font=('Arial',11))   # adds a Label so people know what the programm expects
-                self.label_transfer.grid(column=4, row=[80], padx="10", pady="10")
+            # Anordnung der Unterkategorien pro Kategorie
+            index_keyword = 0
+            for keyword in self.found_categories[category]:
+                check_state = tk.IntVar()
+                check = tk.Checkbutton(self.inner_frame, text=keyword, font=('Arial',10), bg="white", fg="black", variable=check_state)
+                check.grid(row=row_index+1+index_keyword, column=(index_category%4)*2, padx="5", pady="5", sticky=tk.W)
+                index_keyword += 1
+            
+            # Nach jeder vierten Kategorie wird eine neue Zeile begonnen
+            index_category += 1
+            if index_category % 4 == 0:
+                row_index += index_keyword + 40  # +2 für die Lücke zwischen den Kategorien
 
 
 
+        self.canvas.create_window((0,0), window=self.inner_frame, anchor="nw")
+        self.inner_frame.bind("<Configure>", self.resize_canvas)
 
-                # BUTTONS
+        self.root.mainloop()
 
-                # button for NOT adding the new categories to the excel /transfer the data
-                self.button_dont = tk.Button(self.frame_buttons, text="No", relief="groove", font=('Arial',11), bg="lightgrey", command=self.quit)  # if you klick on the button the funktion transfer data will be passed and called whenever the button is klicked
-                self.button_dont.grid(column=4, row=[81], padx="10",sticky=tk.W, pady="50")
+    def resize_canvas(self):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-                # button for adding the new categories to the excel /transfer the data
-                self.button_do = tk.Button(self.frame_buttons, text="Yes", relief="groove", font=('Arial',11), bg="lightgrey", command=self.transfer_categories)  # if you klick on the button the funktion transfer data will be passed and called whenever the button is klicked
-                self.button_do.grid(column=4, row=[81], padx="10",sticky=tk.E, pady="50")
+    
 
-                
-                self.root.mainloop()
-
-        
-
-        def quit(self):
-                self.root.destroy()
+    def quit(self):
+        self.root.destroy()
 
 
-        def transfer_categories(self):
-                categories = {}
-                for category in self.keyword_checkboxes:
-                        for checkbox_status in self.keyword_checkboxes[category]:
-                                if checkbox_status[1].get():
-                                        if category not in categories.keys():
-                                                categories[category] = []
-                                        categories[category].append(checkbox_status[0])
-                                        
-                print(categories)
-                #excel_helper.export_new_categories(categories, self.path_excel)
+    def transfer_categories(self):
+        selected_categories = {}
+        for child in self.inner_frame.winfo_children():
+            if isinstance(child, tk.Checkbutton) and child['variable'].get() == 1:
+                text = child['text']
+                if text in self.found_categories:
+                    selected_categories[text] = [keyword['text'] for keyword in child.master.winfo_children() if isinstance(keyword, tk.Checkbutton) and keyword['variable'].get() == 1]
 
 
-        # this class needs to be callable with a list of categories
-        # Let the user decide which categories he wants
-        # return the modified list
-
+CategorieGUI()
